7189b7a1fe77ab17bd35f796af5595d72c744ce0
jonny rhea <jonathan.rhea@gmail.com>
2021-04-11 14:40:07 -0500

    update ci command

--- construction-paper/pandoc-command.sh
+++
@@ -1,1 +0,0 @@
-pandoc --template=GitHub.html5 -V linkcolor:blue -V fontsize=12pt --standalone --mathjax -f markdown_github+tex_math_dollars -t html5 Construction-Paper.md -o Construction-Paper.html 
